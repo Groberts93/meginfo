7d3fff4fbc04a191dffe154b65c60cd9ae60194f
Gill Roberts <gillian.roberts1993@gmail.com>
2023-08-02 14:47:36 +0000

    Revert "reading data from tags"
    This reverts commit d84f5e6020e05cc8e7b88d34fe4c229cbc31a79c.
    
    reading tag data breaks correct parsing of tag types

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use std::collections::HashMap;
 use std::error::Error;
 use std::fs::File;
 use std::io::{self, Read};
-use std::str;
 
 #[derive(Debug)]
 struct Tag {
@@ -38,24 +37,8 @@ struct TagDef {
     description: String,
 }
 
-#[derive(Debug)]
-enum TagData {
-    Int32(i32),
-    Float(f32),
-    String(String),
-    Unrecognized,
-}
-
-fn parse_tag_data(bytes: &[u8], dtype: &str) -> TagData {
-    match dtype {
-        "int32" => TagData::Int32(i32::from_be_bytes(bytes.try_into().unwrap())),
-        "float" => TagData::Float(f32::from_be_bytes(bytes.try_into().unwrap())),
-        "string" => TagData::String(str::from_utf8(bytes).unwrap().to_string()),
-        _ => TagData::Unrecognized,
-    }
-}
-
 fn main() -> Result<(), Box<dyn Error>> {
+
     // read tags from tsv into a dictionary
     let mut reader = ReaderBuilder::new()
         .delimiter(b'\t')
@@ -76,34 +59,17 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     // read tags sequentially until we find the end (no op) tag
     while let Ok(()) = reader.read_exact(&mut buf) {
-        let (tag, size) = parse_tag_from_bytes(buf);
-
-        let default_tag = &TagDef {
-            code: tag.kind,
-            name: "unknown".to_string(),
-            description: "Unrecognized tag".to_string(),
-            ..Default::default()
-        };
-
-        let tag = tag_dict.get(&tag.kind).unwrap_or(default_tag);
-
-        let mut data_buffer: Vec<u8> = vec![0; size as usize];
-
-        let read_size = match tag.dtype.as_str() {
-            "int32" | "float" | "string" => reader.read(&mut data_buffer).unwrap(),
-            _ => 0,
-        };
-
-        
-        println!("{:?}, {:?}", tag, size);
-
-        if read_size > 0 {
-            let tag_data = parse_tag_data(&data_buffer, tag.dtype.as_str());
-            println!("{:?}", tag_data);
-        } else {
-            reader.seek_relative(size)?;
-        }
-
+        let (tag, size) = parse_tag_from_bytes(&buf);
+        println!(
+            "{:?}",
+            tag_dict.get(&tag.kind).unwrap_or(&TagDef {
+                code: tag.kind,
+                name: "unknown".to_string(),
+                description: "Unrecognized tag".to_string(),
+                ..Default::default()
+            })
+        );
+        reader.seek_relative(size)?;
     }
 
     println!("Finished reading");
@@ -111,7 +77,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn parse_tag_from_bytes(bytes: [u8; 16]) -> (Tag, i64) {
+fn parse_tag_from_bytes(bytes: &[u8; 16]) -> (Tag, i64) {
     let mut ints = [0i32; 4];
 
     for (ii, chunk) in bytes.chunks_exact(4).enumerate() {
