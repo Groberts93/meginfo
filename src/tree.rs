5e83f77845a2a168fa133085325f4dfda93d5943
Gill Roberts <gillian.roberts1993@gmail.com>
2023-08-09 15:46:07 +0000

    removed pointless function and test, moved to using straight vec for nodes instead of option vec

--- src/tree.rs
+++ src/tree.rs
@@ -2,40 +2,34 @@ use crate::tag::{Data, Tag};
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct Tree {
-    nodes: Option<Vec<Tree>>, // can we just get rid of the option?
+    nodes: Vec<Tree>,
     data: Data,
 }
 
 impl Tree {
     pub fn new() -> Self {
         Tree {
-            nodes: None,
+            nodes: vec![],
             data: Data::Void,
         }
     }
 
     pub fn with_data(data: Data) -> Self {
-        Tree { nodes: None, data }
-    }
-
-    pub fn add_node(&mut self, tree: Tree) {
-        match &mut self.nodes {
-            Some(nodes) => nodes.push(tree),
-            None => self.nodes = Some(vec![tree]),
+        Tree {
+            nodes: vec![],
+            data,
         }
     }
 
-    pub fn count_children(tree: &Tree) -> usize {
-        tree.nodes.as_ref().map_or(0, |t| t.len())
+    pub fn add_node(&mut self, tree: Tree) {
+        self.nodes.push(tree);
     }
 
     pub fn count_nodes(tree: &Tree) -> usize {
-        let count = match tree.nodes.as_ref() {
-            Some(trees) => trees.iter().fold(1, |c, t| c + Self::count_nodes(t)),
-            None => 1,
-        };
-
-        count
+        match tree.nodes.len() {
+            0 => 1,
+            _ => tree.nodes.iter().fold(1, |c, t| c + Self::count_nodes(t)),
+        }
     }
 }
 
@@ -45,27 +39,19 @@ mod tests {
 
     #[test]
     fn can_create_root() {
+        // create a root node with no data
         assert_eq!(
             Tree::new(),
             Tree {
-                nodes: None,
+                nodes: vec![],
                 data: Data::Void
             }
         );
     }
 
     #[test]
-    fn can_create_root_with_node() {
-        let mut root = Tree::new();
-        let leaf = Tree::new();
-
-        root.nodes = Some(vec![leaf.clone()]);
-
-        assert_eq!(root.nodes, Some(vec![leaf.clone()]));
-    }
-
-    #[test]
     fn can_add_node() {
+        // create a root node, then add another node as a child
         let mut root = Tree::new();
         let node = Tree::with_data(Data::Float(vec![0.5]));
 
@@ -74,37 +60,20 @@ mod tests {
         assert_eq!(
             root,
             Tree {
-                nodes: Some(vec![Tree {
-                    nodes: None,
+                nodes: vec![Tree {
+                    nodes: vec![],
                     data: Data::Float(vec![0.5])
-                }]),
+                }],
                 data: Data::Void
             }
         );
     }
 
     #[test]
-    fn can_count_child_nodes() {
-        let mut root = Tree::new();
-
-        for _ in 0..4 {
-            root.add_node(Tree::with_data(Data::Void));
-        }
-
-        let child_node = &mut root.nodes.as_mut().unwrap()[0];
-
-        for _ in 0..5 {
-            child_node.add_node(Tree::new());
-        }
-
-        assert_eq!(Tree::count_children(&child_node), 5);
-        assert_eq!(Tree::count_children(&root), 4);
-    }
-
-    #[test]
     fn can_count_tree_nodes() {
+        // create a new tree, add nodes, and check the count is correct
         let mut root = Tree::new();
-        let mut count = 1;
+        let mut count = 1;  // the root node is the only node in the tree
         assert_eq!(Tree::count_nodes(&root), count);
 
         for _ in 0..3 {
@@ -113,7 +82,7 @@ mod tests {
         }
         assert_eq!(Tree::count_nodes(&root), count);
 
-        let child_node = &mut root.nodes.as_mut().unwrap()[0];
+        let child_node = &mut root.nodes[0];
 
         for _ in 0..7 {
             child_node.add_node(Tree::new());
@@ -121,7 +90,7 @@ mod tests {
         }
         assert_eq!(Tree::count_nodes(&root), count);
 
-        let child_node_second = &mut root.nodes.as_mut().unwrap()[0].nodes.as_mut().unwrap()[0];
+        let child_node_second = &mut root.nodes[0].nodes[0];
 
         for _ in 0..2 {
             child_node_second.add_node(Tree::new());
