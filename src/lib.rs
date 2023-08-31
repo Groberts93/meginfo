80d325c3ad7114dc4bee6c22864388f2f7a981c7
Gill Roberts <gillian.roberts1993@gmail.com>
2023-08-31 14:58:08 +0000

    integrated search into main binary

--- src/lib.rs
+++ src/lib.rs
@@ -9,17 +9,25 @@ pub mod parser;
 pub mod query;
 pub mod tag;
 
+use std::collections::HashSet;
+
 use anyhow;
 use config::Config;
 use parser::FifParser;
+use query::Search;
 
 pub fn run(config: Config) -> anyhow::Result<()> {
-    for file in config.files {
-        let tags = FifParser::read_tags(file)?;
-        if config.show_tree {
+    if config.show_tree {
+        for file in config.files {
+            println!("fif tree for {file:?}: \n");
+            let tags = FifParser::read_tags(file)?;
             let tree = FifParser::make_fif_tree(tags)?;
-            println!("{}", tree);
+            println!("{tree}");
         }
+    } else {
+        let mut search = Search::new(HashSet::from_iter(config.query_codes), config.files);
+        search.execute();
+        println!("{search}");
     }
 
     Ok(())
